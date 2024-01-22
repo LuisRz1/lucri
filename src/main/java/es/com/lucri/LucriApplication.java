0cc8724e6d44ab269fc5b92c42fb94ede9537ff0
Luis Angulo Ruiz <143514041+LuisRz1@users.noreply.github.com>
2024-01-22 09:30:37 -0500

    first commit

---
+++ src/main/java/es/com/lucri/LucriApplication.java
@@ -0,0 +1,13 @@
+package es.com.lucri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/com/lucri/LucriApplicationTests.java
@@ -0,0 +1,13 @@
+package es.com.lucri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
